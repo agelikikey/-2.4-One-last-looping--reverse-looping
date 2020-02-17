b611680fb6a1986ae1cf5fe09ccfd5a1ce420b32
agelikikey <agelikikey@gmail.com>
2020-02-17 21:34:42 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let mySampleArray = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie'];
+
+//your code here
+let myNewSampleArray = [];
+  myNewSampleArray.push (' RuthPedro ');
+  myNewSampleArray.push (' Steven ');
+  myNewSampleArray.push   (' Ruth');
+  myNewSampleArray.push (' Lebron ');
+  myNewSampleArray.push (' Pedro ');
+  myNewSampleArray.push (' Maria ');
+  myNewSampleArray.push (' Lou ');
+  myNewSampleArray.push (' Fernando ');
+  myNewSampleArray.push (' Cesco ');
+  myNewSampleArray.push (' Bart ');
+  myNewSampleArray.push (' Pepe ');
+
+// console.log(myNewSampleArray);
+console.log (myNewSampleArray.reverse());
+
+// for(var i=myNewSampleArray.length-1; i>=0; i--) {
+// 	var value = myNewSampleArray[i];
+// 	console.log(i =" "+value);
+// }
